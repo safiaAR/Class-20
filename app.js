99e063b89b6217283d9b06eb998bf4230cc5b7a6
SafiaAR <131646962+safiaAR@users.noreply.github.com>
2023-05-21 12:59:59 +0600

    Add files via upload
    Class-20

---
+++ app.js
@@ -0,0 +1,23 @@
+
+let button =  document.querySelector('nav .container .button')
+
+let sidebar = document.querySelector('.sidebar');
+// <i class="bi bi-x"></i>
+function rato () {
+    sidebar.classList.toggle('active')
+    
+    if(sidebar.classList.contains('active') == true){
+        
+        button.innerHTML = '<i class="bi bi-x-circle"></i>'
+
+    } else{
+        
+        button.innerHTML = '<i class="bi bi-three-dots-vertical"></i>'
+    }
+}
+
+button.addEventListener("click", rato)
+
+
+
+
